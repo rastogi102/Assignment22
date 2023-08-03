70f3d9a27b60f7ed6b49bd0dc9e141d6beade17b
rastogi102 <nikhilrastogi102@gmail.com>
2023-08-03 14:47:14 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Assignment22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Source and Destination classes
+            Source source = new Source
+            {
+                Id = 101,
+                Name = "Tarun Sharma",
+                Age = 30,
+                Address = "Chandni Chowk",
+                City="Delhi"
+            };
+
+            Destination destination = new Destination();
+
+            // Map properties from the source to destination
+            PropertyMapper.MapProperties(source, destination);
+
+            // Display the values of properties in the Destination class to ensure the mapping was successful
+            Console.WriteLine($"Destination Id: {destination.Id}");
+            Console.WriteLine($"Destination Name: {destination.Name}");
+            Console.WriteLine($"Destination Age: {destination.Age}");
+            Console.WriteLine($"Destination City: {destination.City}"); // Additional property in Destination class
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ PropertyMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Assignment22
+{
+    public class PropertyMapper
+    {
+        public static void MapProperties(Source source, Destination destination)
+        {
+            Type sourceType = typeof(Source);
+            Type destinationType = typeof(Destination);
+
+            PropertyInfo[] sourceProperties = sourceType.GetProperties();
+            PropertyInfo[] destinationProperties = destinationType.GetProperties();
+
+            foreach (PropertyInfo sourceProperty in sourceProperties)
+            {
+                PropertyInfo matchingDestinationProperty = Array.Find(destinationProperties, prop => prop.Name == sourceProperty.Name && prop.PropertyType == sourceProperty.PropertyType);
+
+                if (matchingDestinationProperty != null)
+                {
+                    object valueToAssign = sourceProperty.GetValue(source);
+                    matchingDestinationProperty.SetValue(destination, valueToAssign);
+                }
+            }
+        }
+    }
+}
